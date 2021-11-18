a50952459606e6250964f4d077ca9961c83b0f8c
Adam Norris <anorris@rivasolutionsinc.com>
2021-11-18 13:05:02 -0500

    Changed status message.

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/status')
 def status():
-    return 'signal received 5x5', 200
+    return 'glhfdd', 200
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
