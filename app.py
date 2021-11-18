1152e742a502f018039d9ae80756633a46e9f455
Adam Norris <anorris@rivasolutionsinc.com>
2021-11-18 12:11:57 -0500

    Flask microservices app

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/status')
+def status():
+    return 'signal received 5x5', 200
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=True)
